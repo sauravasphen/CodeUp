5248173a04826092501d801ac0f10eaffbc7e205
pradhan21 <75258017+pradhan21@users.noreply.github.com>
2021-05-11 22:41:59 +0545

    work.php made

---
+++ HTML/work.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/work.css">
+    <!-- <link rel="stylesheet" type="text/css" href="css/work.css"> -->
+  </head>
+  <body>
+    .<header>
+      .<h1>Our Work</h1>
+    </header>
+    .<div class="work-container">
+      .<div class="card">
+        .<div class="content">
+          <h2>01</h2>
+          .<h3>Nirvana</h3>
+          .<p>Nirvana Band History</p>
+          <a href="https://sauravasphen.github.io/asphenphotos/index.html">read more</a>
+        </div>
+
+      </div>
+      .<div class="card">
+        .<div class="content">
+          <h2>02</h2>
+          .<h3>Image gallary</h3>
+          .<p>Image Gallary Portfolio</p>
+          <a href="https://sauravasphen.github.io/asphenphotos/index.html">read more</a>
+        </div>
+
+      </div>
+      .<div class="card">
+        .<div class="content">
+          <h2>01</h2>
+          .<h3>Nirvana</h3>
+          .<p>Nirvana Band History</p>
+          <a href="https://sauravasphen.github.io/asphenphotos/index.html">read more</a>
+        </div>
+
+      </div>
+      .<div class="card">
+        .<div class="content">
+          <h2>01</h2>
+          .<h3>Nirvana</h3>
+          .<p>Nirvana Band History</p>
+          <a href="https://sauravasphen.github.io/asphenphotos/index.html">read more</a>
+        </div>
+
+      </div>
+    </div>
+
+
+  </body>
+  </html>
