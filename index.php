8a2c6d31bb2e7bed149b33b2c6db654aad036d6b
sauravasphen <75376341+sauravasphen@users.noreply.github.com>
2021-05-11 11:10:01 +0545

    index php

--- index.php
+++ index.php
@@ -2,11 +2,51 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Welcome</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/home.css">
+    <!-- <link rel="stylesheet" type="text/css" href="css/work.css"> -->
   </head>
   <body>
-    Welcome
-    klufludljfv
-    htxjdhcjh
+    <!-- home  -->
+      <!-- company name  -->
+
+      <header>
+        <video src="MEDIA/tech.mp4" class="vid-big"autoplay loop ></video>
+        <script>
+          document.getElementById('vid').play();
+        </script>
+        .<div class="header-overlay">
+        </div>
+        <div class="banner-text">
+          <h1>CodeUp<h1>
+          <p class="pps">It’s your time to grow through digital marketing
+          </br>and</br>
+           we're here to support u all the way through</p>
+        </div>
+      </header>
+      <br><br><br>
+      <div class="services">
+        <p class="moto">digital agency with solid design and development expertise.<br> We build websites,<br>and elaborate online business services.</p>
+        <br><br><br><h1 class="tts1">Our services</h1>
+        <br><br>
+        <div class="grid-container">
+          <div class="box">
+            <p class="marvel">Logo Design </p>
+          </div>
+          <div class="box">
+            <p class="marvel">Web Design </p>
+          </div>
+          <div class="box">
+            <p class="marvel">Html, css, javascript,php coding </p>
+          </div>
+          <div class="box">
+            <p class="marvel">Social Media handeling</p>
+          </div>
+          <div class="box">
+            <p class="marvel">Social Media Advertising</p>
+          </div>
+        </div>
+      </div>
+
   </body>
 </html>
