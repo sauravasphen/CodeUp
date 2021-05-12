4815905348f2da2f24cdfac6cfe31d4f7135a358
pradhan21 <75258017+pradhan21@users.noreply.github.com>
2021-05-12 13:41:05 +0545

    cc

--- HTML/mainpage2.php
+++ HTML/mainpage2.php
@@ -19,43 +19,11 @@
         <img src="../MEDIA/logo.png" alt="logo">
       </div>
   </div>
-
-    <!-- <header>
-        <video src="MEDIA/touch.mp4" class="vid-big"autoplay loop ></video>
-        <script>
-          document.getElementById('vid').play();
-        </script>
-        <div class="banner-text">
-          <h1>CodeUp<h1>
-          <p class="pps">It’s your time to grow through digital marketi ng
-          </br>and</br>
-           we're here to support u all the way through</p>
-        </div>
-      </header>
-      <br><br><br>
       <div class="services">
         <p class="moto">digital agency with solid design and development expertise.<br> We build websites,<br>and elaborate online business services.</p>
-        <br><br><br><h1 class="tts1">Our services</h1>
-        <br><br>
-        <div class="grid-container">
-          <div class="box">
-            <p class="marvel">Logo Design </p>
-          </div>
-          <div class="box">
-            <p class="marvel">Web Design </p>
-          </div>
-          <div class="box">
-            <p class="marvel">Html, css, javascript,php coding </p>
-          </div>
-          <div class="box">
-            <p class="marvel">Social Media handeling</p>
-          </div>
-          <div class="box">
-            <p class="marvel">Social Media Advertising</p>
-          </div>
-        </div>
+      </div>
         <br><br><br><br>
           <button class="btn-services">Learn More</button>
-      </div> -->
+      </div>
   </body>
 </html>
