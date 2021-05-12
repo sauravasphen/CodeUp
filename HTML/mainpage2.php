f4c2401a08cbfe233a9a8d8a77d821280a9a3995
sauravasphen <75376341+sauravasphen@users.noreply.github.com>
2021-05-12 11:59:57 +0545

    e

--- HTML/mainpage2.php
+++ HTML/mainpage2.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../CSS/mainpage2.css">
-    <!-- <link rel="stylesheet" type="text/css" href="css/work.css"> -->
   </head>
   <body>
     <div class="head-section">
