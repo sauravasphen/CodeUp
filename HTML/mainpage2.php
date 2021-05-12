2e32e88d20c9267fc62ac7c938b12f36a582cb94
pradhan21 <75258017+pradhan21@users.noreply.github.com>
2021-05-12 17:57:27 +0545

    cc

--- HTML/mainpage2.php
+++ HTML/mainpage2.php
@@ -22,8 +22,40 @@
       <div class="services">
         <p class="moto">digital agency with solid design and development expertise.<br> We build websites,<br>and elaborate online business services.</p>
       </div>
+      <div class="container">
+        <div class="box">
+          <p class="marvel">Make your Brand Unique(LOGO designing,digital content) </p>
+          <div class="img">
+          <img src="../MEDIA/logodesign.jpg" class="model">
+        </div>
+        </div>
+
+        <div class="box">
+          <p class="marvel">Let Client know you're digital(social media)</p>
+          <div class="img">
+          <img src="../MEDIA/logodesign.jpg" class="model">
+        </div>
+      </div>
+        <div class="box">
+          <p class="marvel">reach out to more people(Boosting) </p>
+          <div class="img">
+          <img src="../MEDIA/logodesign.jpg" class="model">
+        </div>
+      </div>
+        <div class="box">
+          <p class="marvel">Get yourself a dazling website </p>
+          <div class="img">
+          <img src="../MEDIA/logodesign.jpg" class="model">
+        </div>
+      </div>
+        <div class="box">
+          <p class="marvel">Get easy-breezy digital management portal(Portal)</p>
+          <div class="img">
+          <img src="../MEDIA/logodesign.jpg" class="model">
+        </div>
+      </div>
+      </div>
         <br><br><br><br>
           <button class="btn-services">Learn More</button>
-      </div>
   </body>
 </html>
