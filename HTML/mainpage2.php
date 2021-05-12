53d935c4ada034d7d15bb29dd5465b7685cc4ae6
pradhan21 <75258017+pradhan21@users.noreply.github.com>
2021-05-12 18:02:17 +0545

    cc

--- HTML/mainpage2.php
+++ HTML/mainpage2.php
@@ -19,19 +19,7 @@
         <img src="../MEDIA/logo.png" alt="logo">
       </div>
   </div>
-    <header>
-        <video src="MEDIA/touch.mp4" class="vid-big"autoplay loop ></video>
-        <script>
-          document.getElementById('vid').play();
-        </script>
-        <div class="banner-text">
-          <h1>CodeUp<h1>
-          <p class="pps">It’s your time to grow through digital marketi ng
-          </br>and</br>
-           we're here to support u all the way through</p>
-        </div>
-      </header>
-      <br><br><br>
+  
        <div class="services">
         <p class="moto">digital agency with solid design and development expertise.<br> We build websites,<br>and elaborate online business services.</p>
       </div>
