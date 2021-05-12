dd96de145838c1f142ddb897936dbe78e0c98584
pradhan21 <75258017+pradhan21@users.noreply.github.com>
2021-05-12 11:31:10 +0545

    updated

--- HTML/work.php
+++ HTML/work.php
@@ -7,45 +7,46 @@
     <!-- <link rel="stylesheet" type="text/css" href="css/work.css"> -->
   </head>
   <body>
-    .<header>
-      .<h1>Our Work</h1>
-    </header>
-    .<div class="work-container">
-      .<div class="card">
-        .<div class="content">
+    <div class="header">
+      <h1>Our Work</h1>
+    </div>
+    <br><br>
+    <br><br>
+    <div class="work-container">
+      <div class="card">
+        <div class="content">
           <h2>01</h2>
-          .<h3>Nirvana</h3>
-          .<p>Nirvana Band History</p>
-          <a href="https://sauravasphen.github.io/asphenphotos/index.html">read more</a>
+          <h3>Nirvana</h3>
+          <p>Nirvana Band History</p>
+          <a href="https://pradhan21.github.io/curt-cobain/">read more</a>
         </div>
-
       </div>
-      .<div class="card">
-        .<div class="content">
+
+      <div class="card">
+        <div class="content">
           <h2>02</h2>
-          .<h3>Image gallary</h3>
-          .<p>Image Gallary Portfolio</p>
+          <h3>Image gallary</h3>
+          <p>Image Gallary Portfolio</p>
           <a href="https://sauravasphen.github.io/asphenphotos/index.html">read more</a>
         </div>
-
       </div>
-      .<div class="card">
-        .<div class="content">
-          <h2>01</h2>
-          .<h3>Nirvana</h3>
-          .<p>Nirvana Band History</p>
-          <a href="https://sauravasphen.github.io/asphenphotos/index.html">read more</a>
-        </div>
 
+      <div class="card">
+        <div class="content">
+          <h2>03</h2>
+          <h3>Interactive Resume</h3>
+          <p>A brief website of personal resume</p>
+          <a href="https://aniraj1.github.io/Website/#Contacts">read more</a>
+        </div>
       </div>
-      .<div class="card">
-        .<div class="content">
+
+      <div class="card">
+        <div class="content">
           <h2>01</h2>
-          .<h3>Nirvana</h3>
-          .<p>Nirvana Band History</p>
+          <h3>Nirvana</h3>
+          <p>Nirvana Band History</p>
           <a href="https://sauravasphen.github.io/asphenphotos/index.html">read more</a>
         </div>
-
       </div>
     </div>
 
