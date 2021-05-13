911b35124c3ff22ae1b2333692da3cd688658bb7
sauravasphen <75376341+sauravasphen@users.noreply.github.com>
2021-05-13 08:49:12 +0545

    e

--- HTML/mainpage2.php
+++ HTML/mainpage2.php
@@ -4,13 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../CSS/mainpage2.css">
+    <script type="text/javascript">
+      var vid = document.getElementById('video');
+      vid.autoplay = true;
+      vid.load();
+    </script>
   </head>
   <body>
     <div class="container">
-      <video src="../MEDIA/touch.mp4" autoplay loop id="vid"></video>
-      <script>
-        document.getElementById('vid').play();
-      </script>
+      <video src="../MEDIA/touch.mp4" id  ="video" autoplay loop></video>
       <div class="CodeUp">
         <p>It's time to get digital!</p>
         <h1>CodeUp</h1>
@@ -20,44 +22,44 @@
       </div>
     </div>
     <div class="attract">
-      <div class="box" id="box1" data-color="#d8e3e7">
+      <div class="box" id="box1">
         <div class="text">
           <p>Make your Brand Unique. <br>(Logo designing, digital content)</p>
         </div>
         <div class="image">
-          <video src="../media/design.mp4" autoplay loop>
+          <video src="../media/design.mp4" id="video" autoplay loop>
         </div>
       </div>
-      <div class="box" id="box2" data-color="#51c4d3">
+      <div class="box" id="box2">
         <div class="text">
           <p>Let clients know you're digital. <br>(Social media)</p>
         </div>
         <div class="image">
-          <video src="../media/socialmedia.mp4" autoplay loop>
+          <video src="../media/socialmedia.mp4" id="video" autoplay loop>
         </div>
       </div>
-      <div class="box" id="box3" data-color="#111111">
+      <div class="box" id="box3">
         <div class="text">
           <p>Reach out to more people.<br> (Boosting)</p>
         </div>
         <div class="image">
-          <video src="../media/sales.mp4" autoplay loop>
+          <video src="../media/sales.mp4" id="video" autoplay loop>
         </div>
       </div>
-      <div class="box" id="box4" data-color="#126e82">
+      <div class="box" id="box4">
         <div class="text">
           <p>Get yourself a daziling website!<br>(Website)</p>
         </div>
         <div class="image">
-          <video src="../media/website.mp4" autoplay loop>
+          <video src="../media/website.mp4" id="video" autoplay loop>
         </div>
       </div>
-      <div class="box" id="box5" data-color="#d8e3e7">
+      <div class="box" id="box5" >
         <div class="text">
           <p> Get easy-breezy digital <br>management portal.<br>(Portal)</p>
         </div>
         <div class="image">
-          <video src="../media/portal.mp4" autoplay loop>
+          <video src="../media/portal.mp4" id="video" autoplay loop>
         </div>
       </div>
     </div>
