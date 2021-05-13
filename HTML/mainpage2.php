cb6c184f85e47313205efcc0588b82ef3669c992
sauravasphen <75376341+sauravasphen@users.noreply.github.com>
2021-05-13 10:30:32 +0545

    Minor design changes

--- HTML/mainpage2.php
+++ HTML/mainpage2.php
@@ -4,15 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../CSS/mainpage2.css">
-    <script type="text/javascript">
-      var vid = document.getElementById('video');
-      vid.autoplay = true;
-      vid.load();
-    </script>
   </head>
   <body>
+    <div class="nav-bar">
+      <div class="menu-bar" onclick="">
+
+      </div>
+    </div>
     <div class="container">
-      <video src="../MEDIA/touch.mp4" id  ="video" autoplay loop></video>
+      <video src="../MEDIA/touch.mp4" autoplay loop></video>
       <div class="CodeUp">
         <p>It's time to get digital!</p>
         <h1>CodeUp</h1>
@@ -24,42 +24,47 @@
     <div class="attract">
       <div class="box" id="box1">
         <div class="text">
-          <p>Make your Brand Unique. <br>(Logo designing, digital content)</p>
+          <h1>Make Your Brand Look Unique</h1>
+          <p>Logo Design, Digital Content</p>
         </div>
         <div class="image">
-          <video src="../media/design.mp4" id="video" autoplay loop>
+          <video src="../media/design.mp4" autoplay loop>
         </div>
       </div>
       <div class="box" id="box2">
         <div class="text">
-          <p>Let clients know you're digital. <br>(Social media)</p>
+          <h1>Let Clients know you are Digital.</h1>
+          <p>Social Media, Content Creation</p>
         </div>
         <div class="image">
-          <video src="../media/socialmedia.mp4" id="video" autoplay loop>
+          <video src="../media/socialmedia.mp4" autoplay loop>
         </div>
       </div>
       <div class="box" id="box3">
         <div class="text">
-          <p>Reach out to more people.<br> (Boosting)</p>
+          <h1>Make more sales online.</h1>
+          <p>Digital Advertising</p>
         </div>
         <div class="image">
-          <video src="../media/sales.mp4" id="video" autoplay loop>
+          <video src="../media/sales.mp4" autoplay loop>
         </div>
       </div>
       <div class="box" id="box4">
         <div class="text">
-          <p>Get yourself a daziling website!<br>(Website)</p>
+          <h1>Get you dazling website now.</h1>
+          <p>Web Design, Web Development</p>
         </div>
         <div class="image">
-          <video src="../media/website.mp4" id="video" autoplay loop>
+          <video src="../media/website.mp4" autoplay loop>
         </div>
       </div>
-      <div class="box" id="box5" >
+      <div class="box" id="box5">
         <div class="text">
-          <p> Get easy-breezy digital <br>management portal.<br>(Portal)</p>
+          <h1>Know your Digital status painlessly.</h1>
+          <p>Portal</p>
         </div>
         <div class="image">
-          <video src="../media/portal.mp4" id="video" autoplay loop>
+          <video src="../media/portal.mp4" autoplay loop>
         </div>
       </div>
     </div>
