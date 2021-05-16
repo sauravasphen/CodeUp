d191ceecb8727bcae325bb4c021bb1d9683f2f61
pradhan21 <75258017+pradhan21@users.noreply.github.com>
2021-05-16 14:55:33 +0545

    cc

--- HTML/companytaskhandel.php
+++ HTML/companytaskhandel.php
@@ -1,43 +1,30 @@
 <?php
-SESSION_start();
-
-if(isset($_SESSION['company_id'])){
+session_start();
+$company_id=$_SESSION['c_id'];
+if(isset($_POST['submit'])){
 
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "CodeUp_db";
 
-// Create connection
+
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
   }
-echo "connection successfuly";
 
 $subject=$_POST['subject'];
-$task=$_POST['task'];
+$desc=$_POST['desc'];
 
-// //   $name=$_SESSION['c_name'];
-// //   $selectquery="SELECT c_name FROM Company WHERE c_name==='$name' ;";
-// //   $query=mysqli_query($conn,$selectquery);
-// //   $res=mysqli_fetch_assoc($query);
-// //     if ($res['c_name']===$name) {
-// //        $c_name=$res['c_name'];
-// //     }
-// //     else{
-// //       echo "Error: " . $selectquery . "<br>" . mysqli_error($conn);
-// //     }
-// }
+  $sql= "INSERT INTO Task(company_id,subject,task,issued_date)
+  VALUES($company_id,'$subject','$desc',CURRENT_DATE())";
 
-  $sql= "INSERT INTO Task(company_id,subject,task,status,issued_date)
-  VALUES('$_SESSION['company_id']','$subject','$task',CURRENT_DATE());";
-  $sql .= "INSERT INTO Task(company_id,subject,task,status,issued_date)
-  VALUES('$_SESSION['company_id']','$subject','$task',CURRENT_DATE());";
 
   if(mysqli_multi_query($conn,$sql)){
     echo"task added successfully";
+    header("location:company-task.php");
   }
   else{
     echo "error:".$sql."<br>".mysqli_error($conn);
