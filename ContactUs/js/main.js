595e2578915b8eba9d5d8b5abc33eeb15fd5135f
Forlorn4eva <darko123Z>
2021-05-17 18:40:56 +0545

    contactus2

--- ContactUs/js/main.js
+++ ContactUs/js/main.js
@@ -1,46 +1,35 @@
 
+
 (function ($) {
     "use strict";
 
 
     /*==================================================================
-    [ Focus Contact2 ]*/
-    $('.input100').each(function(){
+    [ Validate after type ]*/
+    $('.validate-input .input100').each(function(){
         $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
-                $(this).addClass('has-val');
+            if(validate(this) == false){
+                showValidate(this);
             }
             else {
-                $(this).removeClass('has-val');
+                $(this).parent().addClass('true-validate');
             }
-        })    
+        })
     })
-  
-  
+
+
     /*==================================================================
     [ Validate ]*/
-    var name = $('.validate-input input[name="name"]');
-    var email = $('.validate-input input[name="email"]');
-    var message = $('.validate-input textarea[name="message"]');
-
+    var input = $('.validate-input .input100');
 
     $('.validate-form').on('submit',function(){
         var check = true;
 
-        if($(name).val().trim() == ''){
-            showValidate(name);
-            check=false;
-        }
-
-
-        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-            showValidate(email);
-            check=false;
-        }
-
-        if($(message).val().trim() == ''){
-            showValidate(message);
-            check=false;
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
         }
 
         return check;
@@ -50,21 +39,42 @@
     $('.validate-form .input100').each(function(){
         $(this).focus(function(){
            hideValidate(this);
-       });
+           $(this).parent().removeClass('true-validate');
+        });
     });
+
+     function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
 
     function showValidate(input) {
         var thisAlert = $(input).parent();
 
         $(thisAlert).addClass('alert-validate');
+
+        $(thisAlert).append('<span class="btn-hide-validate">&#xf136;</span>')
+        $('.btn-hide-validate').each(function(){
+            $(this).on('click',function(){
+               hideValidate(this);
+            });
+        });
     }
 
     function hideValidate(input) {
         var thisAlert = $(input).parent();
-
         $(thisAlert).removeClass('alert-validate');
+        $(thisAlert).find('.btn-hide-validate').remove();
     }
-    
-    
+
+
 
 })(jQuery);
