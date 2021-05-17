55b4d8dcd89691207717b44f6281fde5f1d9afb5
sauravasphen <75376341+sauravasphen@users.noreply.github.com>
2021-05-17 23:10:47 +0545

    Contact us Fetched with database

---
+++ HTML/contactus_handle.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";
+$user = "root";
+$password = "";
+$dbname = "CodeUp_db";
+
+// Create connection
+$conn = mysqli_connect($servername, $user, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+echo "connection successfuly";
+
+$F_name=$_POST['f_name'];
+$company_name=$_POST['company_name'];
+$email=$_POST['email'];
+$contact_num=$_POST['contact_num'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+
+$sql_contactus = "INSERT INTO Contactus(f_name,company_name,email,contact_num,subject,message)
+VALUES('$f_name','$company_name','$email','$contact_num','$subject','$message');";
+
+if (mysqli_query($conn, $sql_contactus)) {
+    echo "Database created successfully";
+    header("location:contactus.php");
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
