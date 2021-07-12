15d83a0c0088347d4a051c76056117eefea19888
pradhan21 <75258017+pradhan21@users.noreply.github.com>
2021-07-12 23:34:45 +0545

    aa

--- HTML/Admin-NewQueries.php
+++ HTML/Admin-NewQueries.php
@@ -12,6 +12,7 @@
      <link rel="stylesheet" href="../css/Admin-Portal.css">
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
      <script type="text/javascript" src="../JS/query-pop.js"></script>
+     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
    </head>
    <body>
      <!-- ************* Navigation ******************* -->
@@ -75,7 +76,7 @@
            <div class="query-item">
              <div class="query-info">
             <h3><?php echo $res['subject']; ?></h3>
-             <?php echo $res['F_name']; ?>
+             <?php echo $res['contactus_id']; ?>
              <br>From <?php echo $res['company_name']; ?>
             </div>
             <div class="query-date">
@@ -89,18 +90,48 @@
               <br>Message: <?php echo $res['message']; ?>
               <br>Phone No: <?php echo $res['contact_num']; ?>
               <br>Message: <?php echo $res['Email']; ?><br>
-              <button type="button" onclick="querypop()">Close</button>
-              <button type="button" onclick="">Delete</button>
+              <span class="action_btn">
+													<a href="#" class="delete delete-btn" data-id="<?php echo $res["contactus_id"]; ?>">Delete</a>
+												</span>
             </div>
-           <div class="query-btn">
-             <button type="button" onclick="querypop()">View</button>
-             <button type="button" onclick="">Delete</button>
-           </div>
          </div>
-         <?php } ?>
+       <?php }
+         ?>
          </div>
        </div>
       </div>
+      <!-- modal starts here -->
+				<div id="delete_model" class="modal">
+						<i class="fas fa-times modal-close"></i>
+
+						<div class="modal-cont">
+							<h2>Are you sure you want to delete?</h2>
+						</div>
+
+						<form action="delete_user.php" method="POST">
+								<input type="hidden" id="Deliting_id" name="Deliting_id">
+								<button class="modal-delete-btn" name="delete">Delete</button>
+						</form>
+				</div>
+
+	         <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"></script>
+
+				<script>
+						$(document).ready(function(){
+							$("#delete_model").hide();
+						  $(".delete-btn").click(function(){
+						    $("#delete_model").toggle();
+						  });
+						  $(".modal-close").click(function(){
+						    $("#delete_model").hide();
+						  });
+						  $(document).on("click",".delete-btn", function(){
+						            var Id = $(this).data("id");
+						            $("#Deliting_id").val(Id);
+						        });
+						});
+				</script>
+
     </body>
   </html>
   <?php
