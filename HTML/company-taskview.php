f804e0cec21057e37df7ae37d4f177f049c2c2e2
Aniraj1 <anirajshahi@gmail.com>
2021-07-13 11:51:35 +0545

    Company Dashboard

--- HTML/company-taskview.php
+++ HTML/company-taskview.php
@@ -9,7 +9,7 @@ $company_id=$_SESSION['company_id'];
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" href="../CSS/admin.css">
+    <link rel="stylesheet" href="../CSS/admin-portal.css">
      <link rel="stylesheet" href="../css/Portal-Navigation.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
   </head>
@@ -29,7 +29,7 @@ $company_id=$_SESSION['company_id'];
           </a>
         </li>
         <li>
-          <a href="company-taskadd.php"
+          <a href="company-taskadd.php">
             <span class="ti-pin-alt"></span>
             <span>Add Task</span>
         </a>
@@ -44,70 +44,61 @@ $company_id=$_SESSION['company_id'];
     </div>
 
     <!-- ***************** Body ********************** -->
-      <div class="content">
-        <h2 class="dash-title">Overview</h2>
-            <div class="activity-card">
-              <h3>Tasks</h3>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Task ID</th>
-                    <th>Subject</th>
-                    <th>Task</th>
-                    <th>Report</th>
-                    <th>Status</th>
-                    <th>Issued Date</th>
-                    <th>Closed Date</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php
-                      $servername = "localhost";
-                      $username = "root";
-                      $password = "";
-                      $dbname = "CodeUp_db";
+    <div class="content">
+          <div class="activity-card">
+            <center><h3>Tasks</h3></center>
+            <table>
+              <thead>
+                <tr>
+                  <th>Task ID</th>
+                  <th>Company ID</th>
+                  <th>Subject</th>
+                  <th>Task</th>
+                  <th>Report</th>
+                  <th>Status</th>
+                  <th>Issued Date</th>
+                  <th>Closed Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $dbname = "CodeUp_db";
 
-                      // Create connection
-                      $conn = mysqli_connect($servername, $username, $password, $dbname);
-                      // Check connection
-                      if (!$conn) {
-                          die("Connection failed: " . mysqli_connect_error());
-                        }
-                        // echo "connection successfuly";
+                    // Create connection
+                    $conn = mysqli_connect($servername, $username, $password, $dbname);
+                    // Check connection
+                    if (!$conn) {
+                        die("Connection failed: " . mysqli_connect_error());
+                      }
 
-                        // $sql="SELECT * FROM Task";
-                        //       $result=mysqli_query($conn,$sql);
-                        //       $row=mysqli_fetch_assoc($result);
-                        //       if($row['company_id']===$company_id){
-                        //         $cmp_id=$row['company_id'];
-                        //
-                        //       }
-                        //       else{
-                        //         echo "error:".$sql."<br>".mysqli_error($conn);
-                        //       }
-                        // if($company_id===$cmp_id){
-                        $selectquery="SELECT *FROM Task where company_id='$company_id'";
-                        $query=mysqli_query($conn,$selectquery);
-                        while($res=mysqli_fetch_array($query)){
-                          ?>
-                          <tr>
-                            <td><?php echo $res['task_id']; ?></td>
-                            <td><?php echo $res['subject']; ?></td>
-                            <td><?php echo $res['task']; ?></td>
-                            <td><?php echo $res['report']; ?></td>
-                            <td><?php echo $res['status']; ?></td>
-                            <td><?php echo $res['issued_date']; ?></td>
-                            <td><?php echo $res['closed_date']; ?></td>
-                          </tr>
-                        <?php
-                        }
-                   ?>
 
-                </tbody>
-              </table>
-            </div>
+                      $selectquery="SELECT *FROM Task";
+                      $query=mysqli_query($conn,$selectquery);
+                      while($res=mysqli_fetch_array($query)){
+                        ?>
+                        <tr>
+                          <td><?php echo $res['task_id']; ?></td>
+                          <td><?php echo $res['company_id']; ?></td>
+                          <td><?php echo $res['subject']; ?></td>
+                          <td><?php echo $res['task']; ?></td>
+                          <td><?php echo $res['report']; ?></td>
+                          <td><?php if ($res['status'] == 0) {echo "Pending";}
+                          else { echo "Finished"; }?></td>
+                          <td><?php echo $res['issued_date']; ?></td>
+                          <td><?php echo $res['closed_date']; ?></td>
+                        </tr>
+                      <?php
+                      }
+                 ?>
 
+              </tbody>
+            </table>
           </div>
+
+  </div>
   </body>
 </html>
 
--- HTML/companypanal.php
+++ HTML/companypanal.php
@@ -2,16 +2,16 @@
   SESSION_start();
 
   if (isset($_SESSION['company_name']) && isset($_SESSION['company_id'])){
-
+  include("connection.php");
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" href="../CSS/admin.css">
-     <link rel="stylesheet" href="../css/Portal-Navigation.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+      <link rel="stylesheet" href="../CSS/admin-portal.css">
+      <link rel="stylesheet" href="../css/Portal-Navigation.css">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
   </head>
   <body>
     <!-- ************* Navigation ******************* -->
@@ -30,7 +30,7 @@
           </a>
         </li>
         <li>
-          <a href="company-taskadd.php"
+          <a href="company-taskadd.php">
             <span class="ti-pin-alt"></span>
             <span>Add Task</span>
         </a>
@@ -45,49 +45,165 @@
     </div>
 
     <!-- ***************** Body ********************** -->
-      <div class="content">
-        <h2 class="dash-title">Overview</h2>
+    <div class="content">
+      <div class="grid-container">
+        <div class="grid-box dash-profile">
+          <h2 class="dash-box-header">Profile</h2>
+          <img src="../MEDIA/profile.png" alt="profile" width="200px"><br />
+          <?php
+          $id = $_SESSION['agent_id'];
+           $sql = "SELECT * FROM agent WHERE agent_id = '$id'";
+           $query = mysqli_query($conn, $sql);
+           while ($res = mysqli_fetch_assoc($query)) {
+          ?>
+          <b>Username: </b><?php echo $res['agent_username']; ?><br>
+          <b>Name: </b><?php echo $res['f_name']; ?><br>
+          <b>Email ID: </b><?php echo $res['Email']; ?><br>
+          <b>Contact No: </b><?php echo $res['contact_num']; ?><br>
+          <?php
+           }
+         ?>
+         <button class="button button1" onclick="openform()"> Change Information </button>
+          <button class="button button2" onclick="changeform()"> Change Password </button>
+        </div>
+        <div class="grid-box dash-agents">
+          <h2 class="dash-box-header"><span class="ti-briefcase"></span>Account Balance</h2>
+          <div class="dash-count">
+              $30,000
+          </div>
+        </div>
+        <div class="grid-box dash-companies">
+           <h2 class="dash-box-header"><span class="ti-reload"></span>Pending Bills</h2>
+           <div class="dash-count">
+              $18,000
+           </div>
+          </div>
+          <div class="grid-box dash-queries">
+            <h2 class="dash-box-header"><span class="ti-check-box"></span>Processed</h2>
+            <div class="dash-count">
+                $18,000
+            </div>
+          </div>
+          <div class="grid-box dash-pending-task">
+            <h2 class="dash-box-header"><span class="ti-check-box"></span>Pending Tasks</h2>
+            <div class="dash-count">
 
-        <div class="dash-cards">
-          <div class="card-single">
-            <div class="card-body">
-              <span class="ti-briefcase"></span>
-              <div class="">
-                <h5>Account balance</h5>
-                <h4>$30,659.45</h4>
-              </div>
-            </div>
-            <div class="card-footer">
-              <a href="">View All</a>
             </div>
           </div>
-          <div class="card-single">
-            <div class="card-body">
-              <span class="ti-reload"></span>
-              <div class="">
-                <h5>Pending</h5>
-                <h4>$19,500.45</h4>
+          <!-- ..........Task List........... -->
+            <div class="grid-box dash-task-list">
+              <h2 class="dash-box-header"><span class="ti-check-box"></span>Tasks</h2>
+            <?php
+            $sql="SELECT *FROM task NATURAL JOIN company ORDER BY task_id DESC LIMIT 3";
+            $query=mysqli_query($conn,$sql);
+            while($res=mysqli_fetch_array($query)){
+              ?>
+              <div class="dash-task">
+                <div class="dash-task-info">
+              <b><?php echo $res['subject']; ?></b><br>
+              &nbsp; &nbsp;For <?php echo $res['company_name']; ?>
+            </div>
+              <div class="dash-task-date">
+                <?php echo $res['issued_date']; ?>
               </div>
             </div>
-            <div class="card-footer">
-              <a href="">View All</a>
-            </div>
+            <?php } ?>
+            <div class="viewtask-button">
+            <a href="agent-taskview.php">View Task</a>
           </div>
-          <div class="card-single">
-            <div class="card-body">
-              <span class="ti-checkbox"></span>
-              <div class="">
-                <h5>Processed</h5>
-                <h4>$19,500.45</h4>
-              </div>
-            </div>
-            <div class="card-footer">
-              <a href="">View All</a>
-            </div>
           </div>
-
         </div>
       </div>
+      <!-- change information -->
+      <div class="popup-overlay"></div>
+     <div class="popup">
+        <div class="popup-close" onclick="closeForm()">×</div>
+       <div class="form">
+         <?php
+
+             $sql="SELECT * FROM agent WHERE agent_id = '$id'";
+             $query = mysqli_query($conn,$sql);
+             if (mysqli_num_rows($query) === 1) {
+             $row =  mysqli_fetch_assoc($query);
+             if($row['agent_id'] === $id){
+           ?>
+           <form action="agent_changeinfo_handle.php" method="POST">
+
+          <div class="header">
+                Change information
+          </div>
+          <hr><br>
+         <div class="element">
+           <label for="fname"><b>Full Name</b></label>
+           <input type="text"  placeholder="Fullname" name="fname" id="fname" value="<?php echo $row["f_name"];   ?>"  required="required">
+       </div>
+   <div class="element">
+     <label for="email"><b>Email</b></label>
+       <input type="email"  placeholder="Email" name="email" id="email" value="<?php echo $row['Email']; ?>" required="required">
+   </div>
+   <div class="element">
+     <label for="phone"><b>Phone number</b></label>
+       <input type="text"  placeholder="Phone number" name="Phone_number" id="phone" value="<?php echo $row['contact_num']; ?>"  required="required">
+   </div>
+   <div class="element">
+     <center><button type="submit" class="button2" name="save">Save changes</button></center>
+   </div>
+  </form>
+   <?php
+       }
+     }
+   ?>
+   </div>
+   </div>
+   <!-- change password -->
+   <div class="popup-overlay"></div>
+     <div class="popup2">
+        <div class="popup2-close" onclick="closeForm1()">×</div>
+       <div class="form">
+
+         <form action="agent_changeinfo_handle.php " method="POST">
+          <div class="header">
+                Change Password
+          </div>
+          <hr><br>
+
+         <div class="element">
+            <label for="cpw"><b>Current Password</b></label>
+             <input type="password"  placeholder="current_password" name="cpw" id="cpw" required="required">
+         </div>
+
+         <div class="element">
+           <label for="npw"><b>New Password</b></label>
+             <input type="password"  placeholder="new_password" name="npw" id="npw" required="required">
+         </div>
+
+         <div class="element">
+           <label for="cpwd"><b>Confirm Password</b></label>
+             <input type="password"  placeholder="confirm_password" name="cpwd" id="cpwd" required="required">
+         </div>
+
+
+         <div class="element">
+           <button type="submit" class="button3" name="savechange">Save changes</button>
+         </div>
+     </form>
+
+   </div>
+  </div>
+  <script>
+  function openform(){
+  document.body.classList.add("showLoginForm");
+  }
+  function changeform(){
+  document.body.classList.add("showchangeForm");
+  }
+  function closeForm(){
+  document.body.classList.remove("showLoginForm");
+  }
+  function closeForm1(){
+  document.body.classList.remove("showchangeForm");
+  }
+  </script>
   </body>
 </html>
 
